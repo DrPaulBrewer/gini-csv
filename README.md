c6eee1d81f3a72140ccbcce2b702906bb05cc99e
Dr Paul Brewer <drpaulbrewer@eaftc.com>
2019-06-08 04:31:32 +0000

    add --sum and reorganize code for 0.2.0.

--- README.md
+++ README.md
@@ -10,7 +10,9 @@ This is a standalone program based on npm:gini-ss, npm:commander, and various cs
 
 ## Installation
 
-For global installation:
+[Install nodejs](https://nodejs.org/en/download/) if you don't have it.  It includes the `node` JavaScript runtime as well as the `npm` package manager.  
+
+To install the gini-csv command for general use:
 
      npm i gini-csv -g
 
@@ -22,10 +24,27 @@ Usage: gini-csv [options] [fromFilePath] [toFilePath]
 Options:
   -m, --match <match>  use column names containing <match> as Gini calculation input columns (required)
   -V, --version        output the version number
+  -s, --sum            sum all rows and calculate Gini coefficient once for entire file
   -n, --nocopy         do not copy Gini input columns to output file
   -v, --verbose        print more status messages
   -h, --help           output usage information
 ```
+
+
+## Examples
+
+    gini-csv -m y profit.csv profitWithGini.csv
+
+Reads from the input file `profit.csv`, matching against columns containing "y" as Gini calculation input columns, and writes
+the output file profitWithGini.csv.
+
+    gini-csv -m SumProfit -n in.csv out.csv
+
+Reads from the input file `in.csv`, matching against columns containing "SumProfit" as Gini calculation input columns, and writes the output file `out.csv`.  The `-n`, short for `--nocopy` causes the Gini input columns to be omitted from the output file.  
+
+    gini-csv -m Participant --sum in.csv out.csv
+
+Reads from the input file `in.csv`, matching against columns containing "Participant" as Gini calculation input columns, and writes the output file `out.csv`.  Each Gini input column will first be summed over all rows in the entire file.  The `out.csv` file will consist of a header row and a single data row resulting from aggregating the relevant matching data and calculating the Gini coefficient.  The columns will be the constant columns in the data, the "*Participant*" columns, and the Gini coefficjent.
 
 ## Small Sample Correction
 
@@ -38,6 +57,12 @@ These converge as the number of samples n become large.
 ## Background
 
 For more information, see the Wikipedia article for [Gini coefficient](https://en.wikipedia.org/wiki/Gini_coefficient)
+
+## Tests
+
+The Gini calculation module used, npm:gini-ss, has its own testing.  Currently there are no additional tests associated
+with the gini-csv program. Tests may be added at a later time
+or if issues independent of gini-ss are reported.
 
 ## Copyright
 
--- bin.js
+++ bin.js
@@ -13,87 +13,182 @@ const stringify = require('csv-stringify');
 const stringifier = stringify();
 const giniSS = require("gini-ss");
 
+const p1 = { parallel: 1 };
 
-(program
-  .version('0.1.0')
-  .arguments('[fromFilePath] [toFilePath]')
-  .option('-n, --nocopy','do not copy Gini input columns to output file')
-  .option('-m, --match <match>', 'use column names containing <match> as Gini calculation input columns')
-  .option('-v, --verbose','print more status messages')
-  .action(function (fromFilePath, toFilePath, options) {
-    const match = options && options.match;
-    const startDate = Date.now();
-    if (options && options.verbose){
-      console.log(new Date(startDate).toUTCString()+" -- "+"gini-csv");
-      console.log("input  file: ", fromFilePath || 'stdin');
-      console.log("output file: ",toFilePath || 'stdout');
-      console.log("match      : "+match);
-      if (options.nocopy) console.log("nocopy     : true, omitting Gini-calculation inputs from output file");
-    }
-    if ((typeof(fromFilePath)==="string") && (fromFilePath.length) && (fromFilePath===toFilePath)) {
-      console.error("Conflict:  fromFilePath and toFilePath must be different");
-      process.exit(1);
-    }
-    if ((typeof(match)!=='string') || (match.length===0)){
-      console.error("Missing: --match <match> substring to identify column names that are Gini calculation inputs");
-      process.exit(1);
-    }
-    let row = 0;
-    let originalHeader = [];
-    let isInputField = [];
-    function spliceGiniColumn(data) {
-      let gini = '';
-      if (row === 0) {
-        originalHeader = data.slice();
-        isInputField = (
-          originalHeader
-          .map((v) => ((typeof(v) === "string") && (v.includes(match))))
-        );
-        if (options && options.verbose){
-          const matching = isInputField.reduce((acc,cv)=>(+acc+cv),0);
-          console.log(matching+" matching columns for Gini-coefficient calculation");
+function passthru(acc, f) {
+  return function (data) {
+    f(acc, data);
+    return data;
+  };
+}
+
+function consumer(acc, f) {
+  return function (data) {
+    f(acc, data);
+    return null;
+  };
+}
+
+function constantFinder(acc) {
+  let row = 0;
+  acc.isConstField = [];
+  return passthru(acc, function (a, d) {
+    if (row === 0) {
+      a.originalHeader = d.slice();
+      a.isConstField = new Array(d.length).fill(true);
+    } else if (row === 1) {
+      a.firstValue = d.map(Number);
+    } else {
+      for (let i = 0, l = d.length; i < l; ++i) {
+        const v = Number(d[i]);
+        if ((a.isConstField[i]) && (v !== a.firstValue[i])){
+          a.isConstField[i] = false;
         }
-        gini = 'gini_ss'
-      } else {
-        const nozero = options && options.nozero;
-        const nonegative = options && options.nonegative;
-        const inData = (
-          data
-          .filter((v, j) => (isInputField[j]))
-          .map((v) => (+v || 0))
-        );
-        gini = giniSS(inData);
       }
-      row++;
-      if (options && options.verbose && (row%1000===0)) console.log("at row: "+row);
-      const outData = (options.nocopy)? (data.filter((v,j)=>(!isInputField[j]))) : (data);
-      outData.push(gini);
-      return outData;
     }
-    const parser = csvparse();
-    // do not change parallel setting from 1, library default was 100 and causing rows to arrive out-of-order
-    const transformer = transform(spliceGiniColumn, {parallel: 1});
-    let iStream=null,oStream=null;
-    if (fromFilePath)
-      iStream = fs.createReadStream(fromFilePath, { encoding: 'utf8' });
-    else
-      iStream = process.stdin;
-    if (toFilePath)
-      oStream = fs.createWriteStream(toFilePath, { encoding: 'utf8' });
-    else
-      oStream = process.stdout;
-    if (options && options.verbose)
-      oStream.on('close', ()=>{
-        console.log("done at row : "+row);
-        console.log("elapsed time: "+(0.001*(Date.now()-startDate)).toFixed(3)+' sec');
-      });
-    (
-      iStream
+    row++;
+  });
+}
+
+function rowZeroMatch(a, match, d) {
+  a.originalHeader = d.slice();
+  a.isInputField = [];
+  for (let i = 0, l = d.length; i < l; ++i) {
+    a.isInputField[i] = d[i].includes(match);
+  }
+}
+
+function hideAndSum(acc, match) {
+  let row = 0;
+  return consumer(acc, function (a, d) {
+    if (row === 0) {
+      rowZeroMatch(a, match, d);
+      a.sum = new Array(d.length).fill(0);
+    } else {
+      for (let i = 0, l = d.length; i < l; ++i) {
+        if (a.isInputField[i]) {
+          a.sum[i] = +(a.sum[i]) + (+d[i]);
+        }
+      }
+    }
+    row++;
+  });
+}
+
+function summer2step(opt) {
+  const match = opt && opt.match;
+  const acc = {};
+  const internalConstantFinder = transform(constantFinder(acc), p1);
+  const transformer = transform(hideAndSum(acc, match), p1);
+  const originalFlusher = transformer._flush.bind(transformer);
+  transformer._flush = function (cb) {
+    originalFlusher(function () {
+      const mergedHeader = acc.originalHeader.filter((v, j) => (
+        acc.isInputField[j] || acc.isConstField[j]
+      ));
+      transformer.push(mergedHeader);
+      const mergeData = [];
+      for (let i = 0, l = acc.originalHeader.length; i < l; ++i) {
+        const relevant = acc.isInputField[i] || acc.isConstField[i];
+        if (relevant) mergeData.push(
+          (acc.isInputField[i]) ? (acc.sum[i]) : (acc.firstValue[i])
+        );
+      }
+      transformer.push(mergeData);
+      cb();
+    });
+  };
+  return [internalConstantFinder,transformer];
+}
+
+function giniRowSplicer(opt) {
+  const acc = {};
+  const match = opt && opt.match;
+  const nocopy = opt && opt.nocopy;
+  let row = 0;
+  return function (data) {
+    let gini = '';
+    if (row === 0) {
+      rowZeroMatch(acc, match, data);
+      gini = 'GiniSS';
+    } else {
+      const inData = (
+        data
+        .filter((v, j) => (acc.isInputField[j]))
+        .map((v) => (+v || 0))
+      );
+      gini = giniSS(inData);
+    }
+    row++;
+    if (opt && opt.verbose && (row % 1000 === 0)) console.log("at row: " + row);
+    const outData = (opt.nocopy) ? (data.filter((v, j) => (!acc.isInputField[j]))) : (data);
+    outData.push(gini);
+    return outData;
+  }
+}
+
+function startPipeline(fromFilePath, toFilePath, options) {
+  const match = options && options.match;
+  const startDate = Date.now();
+  if (options && options.verbose) {
+    console.log(new Date(startDate).toUTCString() + " -- " + "gini-csv");
+    console.log("input  file: ", fromFilePath || 'stdin');
+    console.log("output file: ", toFilePath || 'stdout');
+    console.log("match      : " + match);
+    if (options.sum) console.log("sum        : true, summing over all rows before Gini calculation");
+    if (options.nocopy) console.log("nocopy     : true, omitting Gini-calculation inputs from output file");
+  }
+  if ((typeof(fromFilePath) === "string") && (fromFilePath.length) && (fromFilePath === toFilePath)) {
+    console.error("Conflict:  fromFilePath and toFilePath must be different");
+    process.exit(1);
+  }
+  if ((typeof(match) !== 'string') || (match.length === 0)) {
+    console.error("Missing: --match <match> substring to identify column names that are Gini calculation inputs");
+    process.exit(1);
+  }
+  const doSum = options && options.sum;
+  const parser = csvparse();
+  // do not change parallel setting from 1, library default was 100 and causing rows to arrive out-of-order
+  const transformer = transform(giniRowSplicer(options), p1);
+  let iStream = null,
+    oStream = null;
+  if (fromFilePath)
+    iStream = fs.createReadStream(fromFilePath, { encoding: 'utf8' });
+  else
+    iStream = process.stdin;
+  if (toFilePath)
+    oStream = fs.createWriteStream(toFilePath, { encoding: 'utf8' });
+  else
+    oStream = process.stdout;
+  if (options && options.verbose)
+    oStream.on('finish', () => {
+      console.log("done       : " + (0.001 * (Date.now() - startDate)).toFixed(3) + ' seconds');
+    });
+  if (doSum) {
+    const [findConstStep,sumStep] = summer2step(options);
+    iStream
+      .pipe(parser)
+      .pipe(findConstStep)
+      .pipe(sumStep)
+      .pipe(transformer)
+      .pipe(stringifier)
+      .pipe(oStream);
+  } else {
+    iStream
       .pipe(parser)
       .pipe(transformer)
       .pipe(stringifier)
-      .pipe(oStream)
-    );
-  })
+      .pipe(oStream);
+  }
+}
+
+(program
+  .version('0.2.0')
+  .arguments('[fromFilePath] [toFilePath]')
+  .option('-m, --match <match>', 'use column names containing <match> as Gini calculation input columns')
+  .option('-n, --nocopy', 'do not copy Gini input columns to output file')
+  .option('-s, --sum', 'sum all rows and calculate Gini coefficient once for entire file')
+  .option('-v, --verbose', 'print more status messages')
+  .action(startPipeline)
   .parse(process.argv)
 );
